f7b0604d86264a8657296ef1c5c760e58e71b308
gilhyun.park <gilhyun.park@medihere.com>
2023-05-18 10:12:24 +0900

    chore(bundle 설정 변경): main

--- rollup.config.js
+++ rollup.config.js
@@ -1,43 +1,53 @@
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import image from "@rollup/plugin-image";
-import babel from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import commonjs from "@rollup/plugin-commonjs";
+import terser from "@rollup/plugin-terser";
+import json from "@rollup/plugin-json";
 
 const extensions = ["js", "jsx", "ts", "tsx"];
 const external = ["react", "react-dom", "styled-components"];
 
-process.env.BABEL_ENV = "production";
+function setUpRollup({ input, output }) {
+  return {
+    input,
+    output: {
+      ...output,
+      exports: "named",
+    },
+    watch: {
+      include: "*",
+      exclude: "node_modules/**",
+    },
+    plugins: [
+      peerDepsExternal(),
+      json(),
+      resolve({ extensions }),
+      commonjs(),
+      typescript({ useTsconfigDeclarationDir: true }),
+      image(),
+      terser(),
+    ],
+    external,
+  };
+}
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: "./dist/index.js",
+export default [
+  setUpRollup({
+    input: "src/index.ts",
+    output: {
+      file: "dist/cjs.js",
+      sourcemap: true,
       format: "cjs",
-      sourcemap: true,
     },
-    {
-      file: "./dist/index.es.js",
+  }),
+  setUpRollup({
+    input: "src/index.ts",
+    output: {
+      file: "dist/esm.js",
+      sourcemap: true,
       format: "esm",
-      sourcemap: true,
     },
-  ],
-  plugins: [
-    peerDepsExternal(),
-    resolve({ extensions }),
-    babel({
-      extensions,
-      include: ["packages/src/**/*"],
-      exclude: /node_modules/,
-      babelHelpers: "runtime",
-    }),
-    commonjs({
-      include: /node_modules/,
-    }),
-    typescript({ useTsconfigDeclarationDir: true }),
-    image(),
-  ],
-  external,
-};
+  }),
+];
